93d455557dc1e3c900880140a436af91966c8db4
HETANG404 <hetang401@gmail.com>
2025-05-06 11:42:58 +0800

    first commit

---
+++ src/main/java/com/tang/shadowbacker/ShadowBackerApplication.java
@@ -0,0 +1,13 @@
+package com.tang.shadowbacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowBackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShadowBackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tang/shadowbacker/ShadowBackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tang.shadowbacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadowBackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
